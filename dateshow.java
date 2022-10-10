deff89f0ba83cafc4c74ce50799484b28fbc6b58
MarsUltorR1 <111449923+MarsUltorR1@users.noreply.github.com>
2022-10-10 23:32:50 +0530

    Update dateshow.java

--- dateshow.java
+++ dateshow.java
@@ -1,4 +1,3 @@
-package project;
 import java.util.Scanner;
 public class changedate
 {
@@ -6,11 +5,10 @@ public class changedate
     {
         String array[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
         int leap[]={31,29,31,30,31,30,31,31,30,31,30,31};
-        int nor[]={31,28,31,30,31,30,31,31,30,31,30,31};
         int count=0;
         int day=0;
         if((y%4==0&&y%100!= 0)||y%400 == 0)
-        {
+            leap[1]=29;
             int i=0;
             while(d>=0)
             {
@@ -23,34 +21,12 @@ public class changedate
                 {
                     count++;
                     i++;
-                    
                 }
             }
             day=leap[i]+d;
             if(day==0)
             day=leap[i];
-        }
-        else
-        {
-            int i=0;
-            while(d>=0)
-            {
-                if(i>=12)
-                i=0;
-                if(count>=12)
-                count=0;
-                d=d-nor[i];
-                if(d>0)
-                {
-                    count++;
-                    i++;
-                }
-            }
-            day=nor[i]+d;
-            if(day==0)
-            day=nor[i];
-        }
-        System.out.println(day+" "+array[count]+" "+y);
+        System.out.println(day+" TH "+array[count]+" "+y);
     }
     public static void main(String[] args)
     {
@@ -60,7 +36,6 @@ public class changedate
         int day=sc.nextInt();
         System.out.println("YEAR ");
         int year=sc.nextInt();
-        ob.datechange(day, year);
         System.out.println("Date After(N) Days :");
         int N=sc.nextInt();
         System.out.println("OUTPUT :");
@@ -74,23 +49,21 @@ public class changedate
             System.out.println("Day Number Out Of Range");
             System.exit(0);
         }
+        System.out.println("Date After(N) Days: ");
         ob.datechange(day, year);
         day=day+N;
         if(year%4==0)
-        {
             while(day>366)
             {
                 day=day-366; 
                 year++;  
             }
-        }
         else
         while(day>365)
         {
             day=day-365; 
             year++;  
         }
-        System.out.println("Date After "+N+" Days");
         ob.datechange(day, year);        
     }
 }
